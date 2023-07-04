829e4af674da29cef0c4d264405d169ce0ba090d
Dohyeong Park <slstls218@gmail.com>
2023-07-05 00:53:42 +0900

    push

--- test.js
+++ test.js
@@ -1,15 +1,8 @@
-function Item(title, price) {
-    this.title = title;
-    this.price = price;
-    this.showPrice = function(){
-        console.log(`가격은 ${price} 원 입니다.`);
-    }
+let a = 'age';
 
+const user = {
+    name: 'Mike',
+    [a] : 30
 }
 
-const item1 = new Item("mike",3000);
-const item2 = Item("mek",3000);
-
-console.log(item1,item2);
-
-item1.showPrice();
+console.log(user);
